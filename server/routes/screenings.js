e006275911e7080cfd8d62dd3bd6fc3ce7b176cc
bigjilm <patera@posteo.de>
2019-11-21 23:24:35 +0100

    Add get request for single screening

--- server/routes/screenings.js
+++ server/routes/screenings.js
@@ -7,15 +7,20 @@ router.get('/', (req, res) => {
     .catch(err => res.status(404).json(err))
 })
 
+router.get('/:id', (req, res) => {
+  Screening.findById(req.params.id)
+    .then(screening => res.json(screening))
+    .catch(err => res.status(404).json(err))
+})
+
 router.post('/', (req, res) => {
   Screening.create(req.body)
     .then(newScreening => res.json(newScreening))
     .catch(err => res.status(400).json(err))
 })
 
-router.patch('/', (req, res) => {
-  const { id } = req.query
-  Screening.findByIdAndUpdate(id, req.body)
+router.patch('/:id', (req, res) => {
+  Screening.findByIdAndUpdate(req.params.id, req.body)
     .then(updatedScreening => res.json(updatedScreening))
     .catch(err => res.status(400).json(err))
 })
