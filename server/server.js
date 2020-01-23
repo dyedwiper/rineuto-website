f4a4d594f5fc0cb266c6b155d4694dbdc4025557
bigjilm <patera@posteo.de>
2020-01-23 12:20:27 +0100

    Fix dependency problem after npm audit fix

--- server/server.js
+++ server/server.js
@@ -2,10 +2,11 @@ const express = require('express');
 const app = express();
 const cors = require('cors');
 const mongoose = require('mongoose');
+// const dotenv = require('dotenv');
 
 const port = process.env.PORT || 3333;
 
-app.listen(port, () => console.log('Server ready on port ' + port));
+// dotenv.config();
 
 app.use(express.json());
 app.use(cors());
@@ -14,8 +15,14 @@ app.set('json spaces', 2);
 app.use('/screenings', require('./routes/screenings'));
 app.use('/users', require('./routes/users'));
 
-mongoose.connect('mongodb://localhost:27017/rineuto', {
-  useNewUrlParser: true,
-  useUnifiedTopology: true
-});
+mongoose.connect(
+  'mongodb://localhost:27017/rineuto',
+  {
+    useNewUrlParser: true,
+    useUnifiedTopology: true
+  },
+  () => console.log('Connected to db')
+);
 mongoose.set('useFindAndModify', false);
+
+app.listen(port, () => console.log('Server ready on port ' + port));
