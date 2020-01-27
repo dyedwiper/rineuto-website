2e8238affa74749db783ce7bb99442ceccd72b7c
bigjilm <patera@posteo.de>
2020-01-27 18:20:31 +0100

    Store jwt in local storage on login

--- src/pages/LoginPage.js
+++ src/pages/LoginPage.js
@@ -2,19 +2,37 @@ import React from 'react';
 import styled from 'styled-components/macro';
 import { setToStorage } from '../utils/storage';
 import { useHistory } from 'react-router-dom';
+import { postLoginUser } from '../utils/services';
 
 export default function LoginPage() {
   let history = useHistory();
 
   return (
     <LoginPageStyled>
-      <button onClick={login}>Login</button>
+      <LoginFormStyled onSubmit={handleSubmit}>
+        <InputStyled name="username" />
+        <InputStyled type="password" name="password" />
+        <ButtonStyled>Login</ButtonStyled>
+      </LoginFormStyled>
     </LoginPageStyled>
   );
 
-  function login() {
-    setToStorage('jwt', 'fake');
-    history.push('/intern/addScreening');
+  function handleSubmit(event) {
+    event.preventDefault();
+    const form = event.currentTarget;
+    const formData = new FormData(form);
+    const loginData = Object.fromEntries(formData);
+    postLoginUser(loginData)
+      .then(res => {
+        if (!res.ok) {
+          return res.json().then(err => {
+            throw err;
+          });
+        }
+        setToStorage('jwt', res.headers.get('auth-token'));
+        history.push('/intern/addScreening');
+      })
+      .catch(err => console.error(err));
   }
 }
 
@@ -22,3 +40,13 @@ const LoginPageStyled = styled.main`
   padding: 20px;
   color: white;
 `;
+
+const LoginFormStyled = styled.form`
+  display: grid;
+  grid-auto-rows: min-content;
+  grid-gap: 10px;
+`;
+
+const InputStyled = styled.input``;
+
+const ButtonStyled = styled.button``;
--- src/utils/services.js
+++ src/utils/services.js
@@ -1,18 +1,28 @@
 export function getScreenings() {
-  return fetch('/screenings').then(handleError)
+  return fetch('/screenings').then(handleError);
 }
 
 export function getSingleScreening(id) {
-  return fetch('/screenings/' + id).then(handleError)
+  return fetch('/screenings/' + id).then(handleError);
+}
+
+export function postLoginUser(data) {
+  return fetch('/users/login', {
+    method: 'POST',
+    body: JSON.stringify(data),
+    headers: {
+      'Content-Type': 'application/json'
+    }
+  });
 }
 
 function handleError(res) {
-  let json = res.json()
+  let json = res.json();
   if (!res.ok) {
     return json.then(err => {
-      throw err
-    })
+      throw err;
+    });
   } else {
-    return json
+    return json;
   }
 }
