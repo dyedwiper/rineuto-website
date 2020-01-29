f07c77d4d3b32e27b1ed1c54f80b0ccad5281e67
bigjilm <patera@posteo.de>
2020-01-29 23:36:06 +0100

    Add form for new screenings

--- src/pages/InternPage.js
+++ src/pages/InternPage.js
@@ -1,11 +1,96 @@
 import React from 'react';
 import styled from 'styled-components/macro';
+import { postScreening } from '../utils/services';
+import { getFromStorage } from '../utils/storage';
 
 export default function InternPage() {
-  return <InternPageStyled>intern</InternPageStyled>;
+  return (
+    <InternPageStyled>
+      <HeadlineStyled>Neues Screening anlegen</HeadlineStyled>
+      <FormStyled onSubmit={handleSubmit}>
+        <LabelStyled>
+          Filmtitel
+          <InputStyled name="title" />
+        </LabelStyled>
+        <LabelStyled>
+          Vorführdatum
+          <InputStyled type="date" name="date" />
+        </LabelStyled>
+        <LabelStyled>
+          Regie
+          <InputStyled name="director" />
+        </LabelStyled>
+        <LabelStyled>
+          URL des Bildes
+          <InputStyled name="imageUrl" />
+        </LabelStyled>
+        <LabelStyled>
+          Länge
+          <InputStyled name="length" />
+        </LabelStyled>
+        <LabelStyled>
+          Prodoktionsländer
+          <InputStyled name="country" />
+        </LabelStyled>
+        <LabelStyled>
+          Prodoktionsjahr
+          <InputStyled name="year" />
+        </LabelStyled>
+        <LabelStyled>
+          Version
+          <InputStyled name="version" />
+        </LabelStyled>
+        <LabelStyled>
+          Beschreibung
+          <TextareaStyled name="synopsis" />
+        </LabelStyled>
+        <LabelStyled>
+          Filmreihe
+          <InputStyled name="series" />
+        </LabelStyled>
+        <LabelStyled>
+          Links
+          <InputStyled name="links" />
+        </LabelStyled>
+        <ButtonStyled>Senden</ButtonStyled>
+      </FormStyled>
+    </InternPageStyled>
+  );
+
+  function handleSubmit(event) {
+    event.preventDefault();
+    const form = event.currentTarget;
+    const formData = new FormData(form);
+    const screeningData = Object.fromEntries(formData);
+    const jwt = getFromStorage('rineuto-token');
+    postScreening(screeningData, jwt)
+      .then(res => console.log(res))
+      .catch(err => console.error(err));
+  }
 }
 
 const InternPageStyled = styled.main`
+  overflow: auto;
   padding: 20px;
   color: white;
 `;
+
+const HeadlineStyled = styled.h2`
+  margin: 0 0 10px 0;
+`;
+
+const FormStyled = styled.form`
+  display: grid;
+  grid-gap: 10px;
+`;
+
+const LabelStyled = styled.label`
+  display: grid;
+  grid-auto-rows: min-content;
+`;
+
+const InputStyled = styled.input``;
+
+const TextareaStyled = styled.textarea``;
+
+const ButtonStyled = styled.button``;
--- src/utils/services.js
+++ src/utils/services.js
@@ -6,6 +6,17 @@ export function getSingleScreening(id) {
   return fetch('/screenings/' + id).then(handleError);
 }
 
+export function postScreening(data, token) {
+  return fetch('/screenings', {
+    method: 'POST',
+    body: JSON.stringify(data),
+    headers: {
+      'Content-Type': 'application/json',
+      'auth-token': token
+    }
+  }).then(handleError);
+}
+
 export function postLoginUser(data) {
   return fetch('/users/login', {
     method: 'POST',
