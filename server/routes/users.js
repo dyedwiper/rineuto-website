5b07e9ebd2d71ab4e04b056b86b7926261183388
bigjilm <patera@posteo.de>
2020-04-13 13:45:58 +0200

    Send just username back on login

--- server/routes/users.js
+++ server/routes/users.js
@@ -8,69 +8,69 @@ const authorize = require('../middleware/authorize');
 
 router.get('/', verifyToken, authorize, (req, res) => {
   User.find()
-    .then(users => {
+    .then((users) => {
       res.json(users);
     })
-    .catch(err => res.status(400).json(err));
+    .catch((err) => res.status(400).json(err));
 });
 
 router.get('/verify', verifyToken, (req, res) => {
   User.findById(req.user)
-    .then(user => {
+    .then((user) => {
       if (!user) {
         return res.status(400).json({ error: 'No user for this token' });
       }
       res.json(user);
     })
-    .catch(err => res.status(400).json(err));
+    .catch((err) => res.status(400).json(err));
 });
 
 router.post('/create', verifyToken, authorize, validateUser, (req, res) => {
   User.findOne({ username: req.body.username })
-    .then(user => {
+    .then((user) => {
       if (user) {
         return res.status(400).json({ error: 'username already taken' });
       }
       bcrypt
         .hash(req.body.password, 10)
-        .then(hashedPassword => {
+        .then((hashedPassword) => {
           const newUser = new User({
             username: req.body.username,
             password: hashedPassword,
-            admin: req.body.admin
+            admin: req.body.admin,
           });
           newUser
             .save()
-            .then(newUser =>
+            .then((newUser) =>
               res.json({ success: 'user ' + newUser.username + ' registered' })
             )
-            .catch(err => res.status(400).json(err));
+            .catch((err) => res.status(400).json(err));
         })
-        .catch(err => res.status(400).json(err));
+        .catch((err) => res.status(400).json(err));
     })
-    .catch(err => res.status(400).json(err));
+    .catch((err) => res.status(400).json(err));
 });
 
 router.post('/login', validateLogin, (req, res) => {
   User.findOne({ username: req.body.username })
-    .then(user => {
+    .then((user) => {
       if (!user) {
         return res.status(400).json({ error: 'username does not exist' });
       }
       bcrypt
         .compare(req.body.password, user.password)
-        .then(valid => {
+        .then((valid) => {
           if (!valid) {
             return res.status(400).json({ error: 'password is incorrect' });
           }
           user.lastLogin = Date.now();
           user.save();
           const token = jwt.sign({ _id: user._id }, process.env.JWT_SECRET);
-          res.set('auth-token', token).json(user);
+          res.set('auth-token', token).json({ username: user.username });
         })
-        .catch(err => res.status(400).json(err));
+        .catch((err) => res.status(400).json(err));
     })
-    .catch(err => res.status(400).json(err));
+    .catch((err) => res.status(400).json(err));
 });
 
 module.exports = router;
