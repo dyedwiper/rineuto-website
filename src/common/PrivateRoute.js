9c9406c9d9626675aac291da2abcbec622d859e7
bigjilm <patera@posteo.de>
2020-05-01 19:39:17 +0200

    Update private route

--- src/common/PrivateRoute.js
+++ src/common/PrivateRoute.js
@@ -3,13 +3,13 @@ import { Route, Redirect } from 'react-router-dom';
 import LoadingPage from '../pages/LoadingPage';
 import UserContext from '../userContext';
 
-export default function PrivateRoute({ children, isLoadingUser, ...rest }) {
+export default function PrivateRoute({ children, isLoadingUser }) {
   const { user } = useContext(UserContext);
   const loggedIn = Object.keys(user).length !== 0;
 
-  console.log('loading user', isLoadingUser);
-  console.log('user', user);
-  console.log('loggedIn', loggedIn);
+  // console.log('loading user', isLoadingUser);
+  // console.log('user', user);
+  // console.log('loggedIn', loggedIn);
 
   if (isLoadingUser) {
     return (
@@ -18,9 +18,5 @@ export default function PrivateRoute({ children, isLoadingUser, ...rest }) {
       </Route>
     );
   }
-  return (
-    <Route {...rest}>
-      {loggedIn ? children : <Redirect to="/intern/login" />}
-    </Route>
-  );
+  return <Route>{loggedIn ? children : <Redirect to="/intern/login" />}</Route>;
 }
