fdc3b57278abec916da58cd74487983d0e4fb58b
bigjilm <patera@posteo.de>
2020-06-28 12:47:24 +0200

    Reorder files and refactor npm scripts

--- server/index.js
+++ server/index.js
@@ -17,9 +17,9 @@ app.use(cors());
 app.use(express.static(path.join(__dirname, 'public')));
 
 if (process.env.NODE_ENV === 'production') {
-  app.use(express.static(path.join(__dirname, 'build')));
+  app.use(express.static(path.join(__dirname, '../client/build')));
   app.get(/^(?!\/api)/, (req, res) => {
-    res.sendFile(path.join(__dirname, 'build', 'index.html'));
+    res.sendFile(path.join(__dirname, '../client/build', 'index.html'));
   });
 }
 
