65433e242f7bd1e292a44b260ecf5a0ac9ae620f
bigjilm <patera@posteo.de>
2020-07-23 00:39:44 +0200

    Try: always send meta for screenings

--- server/middleware/sendJustMeta.js
+++ server/middleware/sendJustMeta.js
@@ -2,40 +2,45 @@ const Screening = require('../models/Screening');
 
 function sendJustMeta(req, res, next) {
   const userAgent = req.get('user-agent');
-  if (userAgent && userAgent.includes('facebookexternalhit')) {
-    if (req.path.startsWith('/screening')) {
-      const screeningId = req.path.slice(-24);
-      Screening.findById(screeningId)
-        .then((screening) => {
-          res.send(`
-            <meta property="og:title" content="${screening.title}">
-            <meta property="og:image" content="${screening.imageUrl}">
-            <meta property="og:type" content="website" />
-            <meta property="og:url" content="https://www.rineuto.de" />
-            <meta property="og:site_name" content="Rineuto Lichtspiele" />
-            <meta property="og:description" content="${screening.synopsis}" />
-            <meta property="og:locale" content="de_DE" />
+  // if (userAgent && userAgent.includes('facebookexternalhit')) {
+  if (req.path.startsWith('/screening')) {
+    const screeningId = req.path.slice(-24);
+    Screening.findById(screeningId)
+      .then((screening) => {
+        res.send(`
+            <html>
+              <head>
+                <meta property="og:title" content="${screening.title}">
+                <meta property="og:image" content="${screening.imageUrl}">
+                <meta property="og:type" content="website" />
+                <meta property="og:url" content="https://www.rineuto.de" />
+                <meta property="og:site_name" content="Rineuto Lichtspiele" />
+                <meta property="og:description" content="${screening.synopsis}" />
+                <meta property="og:locale" content="de_DE" />
+              </head>
+            </html>
           `);
-        })
-        .catch((err) => res.status(400).json(err));
-    }
+      })
+      .catch((err) => res.status(400).json(err));
   }
-  if (userAgent && userAgent.includes('Twitterbot')) {
-    if (req.path.startsWith('/screening')) {
-      const screeningId = req.path.slice(-24);
-      Screening.findById(screeningId)
-        .then((screening) => {
-          res.send(`
-            <meta name="twitter:card" content="summary_large_image" />
-            <meta name="twitter:title" content="${screening.title}">
-            <meta name="twitter:description" content="${screening.synopsis}" />
-            <meta name="twitter:image" content="${screening.imageUrl}">
-          `);
-        })
-        .catch((err) => res.status(400).json(err));
-    }
+  // } else if (userAgent && userAgent.includes('Twitterbot')) {
+  //   if (req.path.startsWith('/screening')) {
+  //     const screeningId = req.path.slice(-24);
+  //     Screening.findById(screeningId)
+  //       .then((screening) => {
+  //         res.send(`
+  //           <meta name="twitter:card" content="summary_large_image" />
+  //           <meta name="twitter:title" content="${screening.title}">
+  //           <meta name="twitter:description" content="${screening.synopsis}" />
+  //           <meta name="twitter:image" content="${screening.imageUrl}">
+  //         `);
+  //       })
+  //       .catch((err) => res.status(400).json(err));
+  //   }
+  // }
+  else {
+    next();
   }
-  next();
 }
 
 module.exports = sendJustMeta;
