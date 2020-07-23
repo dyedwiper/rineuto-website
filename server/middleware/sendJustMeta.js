d5c7bea30ca8ea491f1a532115997bf601c3e3f6
bigjilm <patera@posteo.de>
2020-07-23 23:39:05 +0200

    Add other sites to meta middleware

--- server/middleware/sendJustMeta.js
+++ server/middleware/sendJustMeta.js
@@ -8,7 +8,7 @@ function sendJustMeta(req, res, next) {
       Screening.findById(screeningId)
         .then((screening) => {
           res.send(`
-            <meta property="og:title" content="${screening.title}">
+            <meta property="og:title" content="${screening.title} | Rineuto Lichtspiele">
             <meta property="og:image" content="${screening.imageUrl}">
             <meta property="og:type" content="website" />
             <meta property="og:url" content="https://www.rineuto.de/screening/${screeningId}" />
@@ -18,6 +18,56 @@ function sendJustMeta(req, res, next) {
           `);
         })
         .catch((err) => res.status(400).json(err));
+    } else if (req.path.startsWith('/program')) {
+      res.send(`
+        <meta property="og:title" content="Programm | Rineuto Lichtspiele">
+        <meta property="og:image" content="https://www.rineuto.de/filmperlen.jpg">
+        <meta property="og:type" content="website" />
+        <meta property="og:url" content="https://www.rineuto.de/program" />
+        <meta property="og:site_name" content="Rineuto Lichtspiele" />
+        <meta property="og:description" content="Unsere nächsten Filmperlen" />
+        <meta property="og:locale" content="de_DE" />
+      `);
+    } else if (req.path.startsWith('/archive')) {
+      res.send(`
+        <meta property="og:title" content="Archiv | Rineuto Lichtspiele">
+        <meta property="og:image" content="https://www.rineuto.de/filmperlen.jpg">
+        <meta property="og:type" content="website" />
+        <meta property="og:url" content="https://www.rineuto.de/archive" />
+        <meta property="og:site_name" content="Rineuto Lichtspiele" />
+        <meta property="og:description" content="Vergangene Filmperlen" />
+        <meta property="og:locale" content="de_DE" />
+      `);
+    } else if (req.path.startsWith('/posters')) {
+      res.send(`
+        <meta property="og:title" content="Plakate | Rineuto Lichtspiele">
+        <meta property="og:image" content="https://www.rineuto.de/filmperlen.jpg">
+        <meta property="og:type" content="website" />
+        <meta property="og:url" content="https://www.rineuto.de/posters" />
+        <meta property="og:site_name" content="Rineuto Lichtspiele" />
+        <meta property="og:description" content="Selbstgebügelte Plakate" />
+        <meta property="og:locale" content="de_DE" />
+      `);
+    } else if (req.path.startsWith('/about')) {
+      res.send(`
+        <meta property="og:title" content="Über uns | Rineuto Lichtspiele">
+        <meta property="og:image" content="https://www.rineuto.de/filmperlen.jpg">
+        <meta property="og:type" content="website" />
+        <meta property="og:url" content="https://www.rineuto.de/about" />
+        <meta property="og:site_name" content="Rineuto Lichtspiele" />
+        <meta property="og:description" content="Perlen für die Säue" />
+        <meta property="og:locale" content="de_DE" />
+      `);
+    } else if (req.path.startsWith('/contact')) {
+      res.send(`
+        <meta property="og:title" content="Kontakt | Rineuto Lichtspiele">
+        <meta property="og:image" content="https://www.rineuto.de/filmperlen.jpg">
+        <meta property="og:type" content="website" />
+        <meta property="og:url" content="https://www.rineuto.de/contact" />
+        <meta property="og:site_name" content="Rineuto Lichtspiele" />
+        <meta property="og:description" content="Adresse etc." />
+        <meta property="og:locale" content="de_DE" />
+      `);
     } else {
       next();
     }
@@ -34,6 +84,41 @@ function sendJustMeta(req, res, next) {
           `);
         })
         .catch((err) => res.status(400).json(err));
+    } else if (req.path.startsWith('/program')) {
+      res.send(`
+        <meta name="twitter:card" content="summary_large_image" />
+        <meta name="twitter:title" content="Programm | Rineuto Lichtspiele">
+        <meta name="twitter:description" content="Unsere nächsten Filmperlen" />
+        <meta name="twitter:image" content="https://www.rineuto.de/filmperlen.jpg">
+      `);
+    } else if (req.path.startsWith('/archive')) {
+      res.send(`
+        <meta name="twitter:card" content="summary_large_image" />
+        <meta name="twitter:title" content="Archiv | Rineuto Lichtspiele">
+        <meta name="twitter:description" content="Vergangene Filmperlen" />
+        <meta name="twitter:image" content="https://www.rineuto.de/filmperlen.jpg">
+      `);
+    } else if (req.path.startsWith('/posters')) {
+      res.send(`
+        <meta name="twitter:card" content="summary_large_image" />
+        <meta name="twitter:title" content="Plakate | Rineuto Lichtspiele">
+        <meta name="twitter:description" content="Selbstgebügelte Plakate" />
+        <meta name="twitter:image" content="https://www.rineuto.de/filmperlen.jpg">
+      `);
+    } else if (req.path.startsWith('/about')) {
+      res.send(`
+        <meta name="twitter:card" content="summary_large_image" />
+        <meta name="twitter:title" content="Über uns | Rineuto Lichtspiele">
+        <meta name="twitter:description" content="Perlen für die Säue" />
+        <meta name="twitter:image" content="https://www.rineuto.de/filmperlen.jpg">
+      `);
+    } else if (req.path.startsWith('/contact')) {
+      res.send(`
+        <meta name="twitter:card" content="summary_large_image" />
+        <meta name="twitter:title" content="Kontakt | Rineuto Lichtspiele">
+        <meta name="twitter:description" content="Adresse etc." />
+        <meta name="twitter:image" content="https://www.rineuto.de/filmperlen.jpg">
+      `);
     } else {
       next();
     }
