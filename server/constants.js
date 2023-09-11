ae4edbf54564b5a55b94bfd8ea1d8d3c58faa74c
dyedwiper <patera@posteo.de>
2023-09-11 15:47:00 +0200

    Add constants

---
+++ server/constants.js
@@ -0,0 +1,46 @@
+const RINEUTO_NAME = 'Rineuto Lichtspiele';
+
+const RINEUTO_BASE_URL = 'https://www.rineuto.de';
+
+const PEARLS_IMAGE_FILENAME = 'filmperlen.jpg';
+
+const HEADLINE_ABOUT = 'Perlen für die Säue';
+const HEADLINE_ARCHIV = 'Vergangene Filmperlen';
+const HEADLINE_CONTACT = 'Adresse etc.';
+const HEADLINE_POSTERS = 'Selbstgebügelte Plakate';
+const HEADLINE_PROGRAM = 'Unsere nächsten Filmperlen';
+
+const PAGE_TITLE_ABOUT = 'Über uns';
+const PAGE_TITLE_ARCHIVE = 'Archiv';
+const PAGE_TITLE_CONTACT = 'Kontakt';
+const PAGE_TITLE_POSTERS = 'Plakate';
+const PAGE_TITLE_PROGRAM = 'Programm';
+
+const ROUTE_ABOUT = '/about';
+const ROUTE_ARCHIVE = '/archive';
+const ROUTE_CONTACT = '/contact';
+const ROUTE_POSTERS = '/posters';
+const ROUTE_PROGRAM = '/program';
+const ROUTE_SCREENING = '/screening';
+
+module.exports = {
+  RINEUTO_NAME,
+  HEADLINE_ABOUT,
+  HEADLINE_ARCHIV,
+  HEADLINE_CONTACT,
+  HEADLINE_POSTERS,
+  HEADLINE_PROGRAM,
+  RINEUTO_BASE_URL,
+  PEARLS_IMAGE_FILENAME,
+  PAGE_TITLE_ABOUT,
+  PAGE_TITLE_ARCHIVE,
+  PAGE_TITLE_CONTACT,
+  PAGE_TITLE_POSTERS,
+  PAGE_TITLE_PROGRAM,
+  ROUTE_ABOUT,
+  ROUTE_ARCHIVE,
+  ROUTE_CONTACT,
+  ROUTE_POSTERS,
+  ROUTE_PROGRAM,
+  ROUTE_SCREENING,
+};
--- server/middleware/sendJustMeta.js
+++ server/middleware/sendJustMeta.js
@@ -1,3 +1,25 @@
+const path = require('path');
+const {
+  RINEUTO_BASE_URL,
+  ROUTE_SCREENING,
+  PEARLS_IMAGE_FILENAME,
+  RINEUTO_NAME,
+  ROUTE_PROGRAM,
+  HEADLINE_PROGRAM,
+  ROUTE_ARCHIVE,
+  PAGE_TITLE_PROGRAM,
+  PAGE_TITLE_ARCHIVE,
+  HEADLINE_ARCHIV,
+  HEADLINE_POSTERS,
+  PAGE_TITLE_POSTERS,
+  PAGE_TITLE_ABOUT,
+  ROUTE_ABOUT,
+  HEADLINE_ABOUT,
+  PAGE_TITLE_CONTACT,
+  ROUTE_CONTACT,
+  HEADLINE_CONTACT,
+  ROUTE_POSTERS,
+} = require('../constants');
 const Screening = require('../models/Screening');
 const { STANDARD_ERROR_MESSAGE } = require('../utils/constants');
 
@@ -5,6 +27,9 @@ const USER_AGENT_FACEBOOK = 'facebookexternalhit';
 const USER_AGENT_TELEGRAM = 'TelegramBot';
 const USER_AGENT_TWITTER = 'Twitterbot';
 const USER_AGENT_WHATSAPP = 'WhatsApp';
+
+const OG_TYPE = 'website';
+const OG_LOCALE = 'de_DE';
 
 function sendJustMeta(req, res, next) {
   const userAgent = req.header('user-agent');
@@ -15,7 +40,7 @@ function sendJustMeta(req, res, next) {
       userAgent.includes(USER_AGENT_TWITTER) ||
       userAgent.includes(USER_AGENT_WHATSAPP))
   ) {
-    if (req.path.startsWith('/screening')) {
+    if (req.path.startsWith(ROUTE_SCREENING)) {
       const screeningId = req.path.slice(req.path.indexOf('screening') + 10, req.path.indexOf('screening') + 34);
       Screening.findById(screeningId)
         .then((screening) => {
@@ -23,65 +48,65 @@ function sendJustMeta(req, res, next) {
             return res.status(404).json('screening not found');
           }
           res.send(`
-            <meta property="og:title" content="${screening.title} | Rineuto Lichtspiele">
+            <meta property="og:title" content="${screening.title} | ${RINEUTO_NAME}">
             <meta property="og:image" content="${screening.imageUrl}">
-            <meta property="og:type" content="website" />
-            <meta property="og:url" content="https://www.rineuto.de/screening/${screeningId}" />
-            <meta property="og:site_name" content="Rineuto Lichtspiele" />
+            <meta property="og:type" content="${OG_TYPE}" />
+            <meta property="og:url" content="${path.join(RINEUTO_BASE_URL, ROUTE_SCREENING, screeningId)}" />
+            <meta property="og:site_name" content="${RINEUTO_NAME}" />
             <meta property="og:description" content="${screening.synopsis}" />
-            <meta property="og:locale" content="de_DE" />
+            <meta property="og:locale" content="${OG_LOCALE}" />
           `);
         })
         .catch(() => res.status(500).json(STANDARD_ERROR_MESSAGE));
-    } else if (req.path.startsWith('/program')) {
+    } else if (req.path.startsWith(ROUTE_PROGRAM)) {
       res.send(`
-        <meta property="og:title" content="Programm | Rineuto Lichtspiele">
-        <meta property="og:image" content="https://www.rineuto.de/filmperlen.jpg">
-        <meta property="og:type" content="website" />
-        <meta property="og:url" content="https://www.rineuto.de/program" />
-        <meta property="og:site_name" content="Rineuto Lichtspiele" />
-        <meta property="og:description" content="Unsere nächsten Filmperlen" />
-        <meta property="og:locale" content="de_DE" />
+        <meta property="og:title" content="${PAGE_TITLE_PROGRAM} | ${RINEUTO_NAME}">
+        <meta property="og:image" content="${path.join(RINEUTO_BASE_URL, PEARLS_IMAGE_FILENAME)}">
+        <meta property="og:type" content="${OG_TYPE}" />
+        <meta property="og:url" content="${path.join(RINEUTO_BASE_URL, ROUTE_PROGRAM)}" />
+        <meta property="og:site_name" content="${RINEUTO_NAME}" />
+        <meta property="og:description" content="${HEADLINE_PROGRAM}" />
+        <meta property="og:locale" content="${OG_LOCALE}" />
       `);
-    } else if (req.path.startsWith('/archive')) {
+    } else if (req.path.startsWith(ROUTE_ARCHIVE)) {
       res.send(`
-        <meta property="og:title" content="Archiv | Rineuto Lichtspiele">
-        <meta property="og:image" content="https://www.rineuto.de/filmperlen.jpg">
-        <meta property="og:type" content="website" />
-        <meta property="og:url" content="https://www.rineuto.de/archive" />
-        <meta property="og:site_name" content="Rineuto Lichtspiele" />
-        <meta property="og:description" content="Vergangene Filmperlen" />
-        <meta property="og:locale" content="de_DE" />
+        <meta property="og:title" content="${PAGE_TITLE_ARCHIVE} | ${RINEUTO_NAME}">
+        <meta property="og:image" content="${path.join(RINEUTO_BASE_URL, PEARLS_IMAGE_FILENAME)}">
+        <meta property="og:type" content="${OG_TYPE}" />
+        <meta property="og:url" content="${path.join(RINEUTO_BASE_URL, ROUTE_ARCHIVE)}" />
+        <meta property="og:site_name" content="${RINEUTO_NAME}" />
+        <meta property="og:description" content="${HEADLINE_ARCHIV}" />
+        <meta property="og:locale" content="${OG_LOCALE}" />
       `);
-    } else if (req.path.startsWith('/posters')) {
+    } else if (req.path.startsWith(ROUTE_POSTERS)) {
       res.send(`
-        <meta property="og:title" content="Plakate | Rineuto Lichtspiele">
-        <meta property="og:image" content="https://www.rineuto.de/filmperlen.jpg">
-        <meta property="og:type" content="website" />
-        <meta property="og:url" content="https://www.rineuto.de/posters" />
-        <meta property="og:site_name" content="Rineuto Lichtspiele" />
-        <meta property="og:description" content="Selbstgebügelte Plakate" />
-        <meta property="og:locale" content="de_DE" />
+        <meta property="og:title" content="${PAGE_TITLE_POSTERS} | ${RINEUTO_NAME}">
+        <meta property="og:image" content="${path.join(RINEUTO_BASE_URL, PEARLS_IMAGE_FILENAME)}">
+        <meta property="og:type" content="${OG_TYPE}" />
+        <meta property="og:url" content="${path.join(RINEUTO_BASE_URL, ROUTE_POSTERS)}" />
+        <meta property="og:site_name" content="${RINEUTO_NAME}" />
+        <meta property="og:description" content="${HEADLINE_POSTERS}" />
+        <meta property="og:locale" content="${OG_LOCALE}" />
       `);
-    } else if (req.path.startsWith('/about')) {
+    } else if (req.path.startsWith(ROUTE_ABOUT)) {
       res.send(`
-        <meta property="og:title" content="Über uns | Rineuto Lichtspiele">
-        <meta property="og:image" content="https://www.rineuto.de/filmperlen.jpg">
-        <meta property="og:type" content="website" />
-        <meta property="og:url" content="https://www.rineuto.de/about" />
-        <meta property="og:site_name" content="Rineuto Lichtspiele" />
-        <meta property="og:description" content="Perlen für die Säue" />
-        <meta property="og:locale" content="de_DE" />
+        <meta property="og:title" content="${PAGE_TITLE_ABOUT} | ${RINEUTO_NAME}">
+        <meta property="og:image" content="${path.join(RINEUTO_BASE_URL, PEARLS_IMAGE_FILENAME)}">
+        <meta property="og:type" content="${OG_TYPE}" />
+        <meta property="og:url" content="${path.join(RINEUTO_BASE_URL, ROUTE_ABOUT)}" />
+        <meta property="og:site_name" content="${RINEUTO_NAME}" />
+        <meta property="og:description" content="${HEADLINE_ABOUT}" />
+        <meta property="og:locale" content="${OG_LOCALE}" />
       `);
-    } else if (req.path.startsWith('/contact')) {
+    } else if (req.path.startsWith(ROUTE_CONTACT)) {
       res.send(`
-        <meta property="og:title" content="Kontakt | Rineuto Lichtspiele">
-        <meta property="og:image" content="https://www.rineuto.de/filmperlen.jpg">
-        <meta property="og:type" content="website" />
-        <meta property="og:url" content="https://www.rineuto.de/contact" />
-        <meta property="og:site_name" content="Rineuto Lichtspiele" />
-        <meta property="og:description" content="Adresse etc." />
-        <meta property="og:locale" content="de_DE" />
+        <meta property="og:title" content="${PAGE_TITLE_CONTACT} | ${RINEUTO_NAME}">
+        <meta property="og:image" content="${path.join(RINEUTO_BASE_URL, PEARLS_IMAGE_FILENAME)}">
+        <meta property="og:type" content="${OG_TYPE}" />
+        <meta property="og:url" content="${path.join(RINEUTO_BASE_URL, ROUTE_CONTACT)}" />
+        <meta property="og:site_name" content="${RINEUTO_NAME}" />
+        <meta property="og:description" content="${HEADLINE_CONTACT}" />
+        <meta property="og:locale" content="${OG_LOCALE}" />
       `);
     } else {
       next();
