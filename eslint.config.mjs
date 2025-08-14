ad110f2a33d24acc732808e1aaa7ae66264488d0
dyedwiper <patera@posteo.de>
2025-08-14 20:18:36 +0200

    Reorder imports

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,7 +1,7 @@
 import js from '@eslint/js';
 import reactPlugin from 'eslint-plugin-react';
-import { defineConfig } from 'eslint/config';
 import reactHooksPlugin from 'eslint-plugin-react-hooks';
+import { defineConfig } from 'eslint/config';
 import globals from 'globals';
 
 export default defineConfig([
