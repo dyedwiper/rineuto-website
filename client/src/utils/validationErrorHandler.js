f9b04b05041dc2b9dde5cf9de688e7ee5b445730
dyedwiper <patera@posteo.de>
2025-08-24 21:20:03 +0200

    Update validationErrorHandler

--- client/src/utils/validationErrorHandler.js
+++ client/src/utils/validationErrorHandler.js
@@ -2,11 +2,9 @@ export function handleValidationError(error, setValidationError) {
   const errorResponseData = error?.response?.data;
   if (Object.hasOwn(errorResponseData, 'joiError')) {
     setValidationError(errorResponseData.joiError);
-  }
-  if (Object.hasOwn(errorResponseData, 'multerError')) {
+  } else if (Object.hasOwn(errorResponseData, 'multerError')) {
     setValidationError(errorResponseData.multerError);
-  }
-  if (Object.hasOwn(errorResponseData, 'cloudinaryError')) {
-    setValidationError(errorResponseData.cloudinaryError);
+  } else {
+    window.location = '/error';
   }
 }
