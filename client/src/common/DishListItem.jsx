3253723ce07ad8254a7cd97cbdae7893a2f1d60b
dyedwiper <patera@posteo.de>
2025-10-12 14:26:59 +0200

    Add DeletePrompt when deleting dishes

--- client/src/common/DishListItem.jsx
+++ client/src/common/DishListItem.jsx
@@ -1,17 +1,21 @@
-import React, { useContext } from 'react';
+import React, { useContext, useState } from 'react';
 import styled from 'styled-components';
 import Context from '../Context';
 import { deleteDish } from '../services/dishServices';
 import { formatToDateString } from '../utils/dateFormatters';
+import DeletePrompt from './DeletePrompt';
 
 export default function DishListItem({ dish, dishes, setDishes }) {
+  const [showDeletePrompt, setShowDeletePrompt] = useState(false);
+
   const { isUserLoggedIn, setIsWaiting } = useContext(Context);
 
   return (
     <DishListItemStyled>
       <DateStyled>{formatToDateString(dish.date)}</DateStyled>
       <NameStyled>{dish.name}</NameStyled>
-      {isUserLoggedIn && <DeleteButtonStyled onClick={handleDelete}>Löschen</DeleteButtonStyled>}
+      {isUserLoggedIn && <DeleteButtonStyled onClick={() => setShowDeletePrompt(true)}>Löschen</DeleteButtonStyled>}
+      {showDeletePrompt && <DeletePrompt handleDelete={handleDelete} setShowDeletePrompt={setShowDeletePrompt} />}
     </DishListItemStyled>
   );
 
