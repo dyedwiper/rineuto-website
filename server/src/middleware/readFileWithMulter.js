024d327d287d97f92bb694437b6645e025a876be
dyedwiper <patera@posteo.de>
2026-03-01 09:31:53 +0100

    Upgrade multer

--- server/src/middleware/readFileWithMulter.js
+++ server/src/middleware/readFileWithMulter.js
@@ -7,6 +7,7 @@ function fileFilter(req, file, cb) {
   if (file.mimetype !== 'image/jpeg' && file.mimetype !== 'image/png') {
     return cb(new Error(ERROR_MESSAGE_MIMETYPE), false);
   }
+
   cb(null, true);
 }
 
@@ -16,12 +17,14 @@ function readFileWithMulter(req, res, next) {
     fileFilter: fileFilter,
     storage: multer.memoryStorage(),
   }).single('image');
+
   upload(req, res, function (err) {
     if (err?.message === ERROR_MESSAGE_MIMETYPE || err?.code === 'LIMIT_FILE_SIZE') {
       return res.status(422).json({ multerError: err.message });
     } else if (err) {
       return res.status(500).json(STANDARD_ERROR_MESSAGE);
     }
+
     next();
   });
 }
